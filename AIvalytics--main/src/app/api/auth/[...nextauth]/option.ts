92a37668448da959f54d9ddc76a8ac4ca96491d8
chetan21122004 <chetan44rt@gmail.com>
2025-03-25 20:22:12 +0530

    implemented supa base auth with help of Nitesh :)

--- AIvalytics--main/src/app/api/auth/[...nextauth]/option.ts
+++
@@ -1,5 +0,0 @@
-import NextAuthOptions from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcryptjs"
-import dbConnect from "@/lib/dbConnect"
-import Teacher from "../../models/teacherModel"
